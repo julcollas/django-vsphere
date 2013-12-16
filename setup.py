63abc863ec6f9e0be86613b12a0f83f3a657742b
Julien <jul.collas@gmail.com>
2013-12-16 14:13:01 +0100

    Update setup.py
    update version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name = 'django-vsphere',
-    version = '0.2',
+    version = '0.3',
     packages = ['vsphere'],
     include_package_data = True,
     description = 'Django app for vSphere Dashboard.',
