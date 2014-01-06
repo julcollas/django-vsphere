f8b0e32dd1d90fff807faf10bf52dbe123461db1
Julien <jul.collas@gmail.com>
2014-01-06 22:35:13 +0100

    increase version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name = 'django-vsphere',
-    version = '0.3',
+    version = '0.4',
     packages = ['vsphere'],
     include_package_data = True,
     description = 'Django app for vSphere Dashboard.',
